bc4c6b4c89c064e10e7d9b5471cc74cb2936fabc
DESKTOP-0UPI14J\hp <mmbalisi@csmbataan.ph.education>
2020-12-13 16:51:10 +0800

    Adding a DataGrid named dtgNews.

--- MarkBalisi.SecondQuarterlyExam/MarkBalisi.SecondQuarterlyExam/MainWindow.xaml.cs
+++ MarkBalisi.SecondQuarterlyExam/MarkBalisi.SecondQuarterlyExam/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace MarkBalisi.SecondQuarterlyExam
         {
             InitializeComponent();
         }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
