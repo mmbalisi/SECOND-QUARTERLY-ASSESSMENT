db271fecc431c4571ebf631df5ba09dac5688f24
DESKTOP-0UPI14J\hp <mmbalisi@csmbataan.ph.education>
2020-12-14 21:27:32 +0800

    Added a public class named News.

--- MarkBalisi.SecondQuarterlyExam/MarkBalisi.SecondQuarterlyExam/MainWindow.xaml.cs
+++ MarkBalisi.SecondQuarterlyExam/MarkBalisi.SecondQuarterlyExam/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Net;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RestSharp;
+using Newtonsoft.Json;
 
 namespace MarkBalisi.SecondQuarterlyExam
 {
@@ -20,14 +23,17 @@ namespace MarkBalisi.SecondQuarterlyExam
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string resultAsJson;
+
         public MainWindow()
         {
-            InitializeComponent();
-        }
+            var url = "http://newsapi.org/v2/top-headlines?country=ar&category=business&apiKey=21abf07344d6405e8b0c8a04d2de23b8" +
+          "country=ar&" +
+          "apiKey=21abf07344d6405e8b0c8a04d2de23b8";
 
-        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+            var json = new WebClient().DownloadString(url);
 
+            Console.WriteLine(json);
         }
     }
 }
